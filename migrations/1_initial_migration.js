4ca3aee150be5d6bdcdd4fe510b05c79bfcaf2f7
Nissan Gabay <nissan@valid.network>
2020-05-03 15:10:23 +0300

    v2

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,5 +2,5 @@ const Migrations = artifacts.require('./Migrations.sol');
 
 module.exports = function(deployer, network, accounts) {
 	const deployerAcct = accounts[0];
-	deployer.deploy(Migrations, { from: deployerAcct });
+	deployer.deploy(Migrations, { from: deployerAcct });  
 };
